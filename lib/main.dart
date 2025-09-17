7e1ce03a673f2ea7ae73b11db55951a49f8d1a3f
borisbokan <borisbokan@gmail.com>
2025-09-17 22:16:20 +0200

    Some changes with print or showing result of calculation in PercentCalculation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flubo_package/flubo_package.dart';
 
 void main() {
-  //List<double> pricesList = [1890.0, 560.0, 780.0, 256.0, 1450.0];
+  List<double> pricesList = [1890.0, 560.0, 780.0, 256.0, 1450.0];
 
   // pricesList.forEach((item) {
   //   final calItem = PriceCalculation(startValue: item, gain: 20);
@@ -10,11 +10,22 @@ void main() {
   //   print(calItem.toString());
   // });
 
-  var perFromVal = PercentCalulations(
-    mainValue: 1450,
-    amount: 545,
-    percent: 54,
-  );
-  perFromVal.calculate();
-  print(perFromVal.toString());
+  // var perFromVal = PercentCalulations(
+  //   mainValue: 1450,
+  //   amount: null,
+  //   percent: 24,
+  // );
+  // perFromVal.calculate();
+  // print(perFromVal.toString());
+
+  pricesList.forEach((item) {
+    final calItem = PercentCalulations(
+      mainValue: item,
+      percent: 13,
+      amount: null,
+    );
+    calItem.calculate();
+
+    print(calItem.toString());
+  });
 }
